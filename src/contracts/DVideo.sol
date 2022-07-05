9acf53f3a714f377e0bcbde39b347f80d5d4499d
Saksham Garg <sakshamgarg4957@gmail.com>
2022-07-05 14:27:17 +0530

    Smart Contract Complete

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,49 @@
 pragma solidity ^0.5.0;
 
 contract DVideo {
-  uint public videoCount = 0;
-  string public name = "DVideo";
-  //Create id=>struct mapping
+	uint public videoCount = 0;
+	string public name = "DVideo";
+	//2. Storing the videos using mapping
+	//Create id=>struct mapping
+	mapping(uint => Video) public videos;
 
-  //Create Struct
+	//1. Model the video
+	//Create Struct
+	struct Video{
+		uint id; //unique identifier for this video
+		string hash; //hash for where the video is stored on IPFS (Inter Planetary File System)
+		string title; //title of the video
+		address author; //address of the person who uploaded this video
+	}
 
+	//Create Event
+	event VideoUploaded(
+		uint id,
+		string hash,
+		string title,
+		address author
+	);
 
-  //Create Event
+	constructor() public {
+	}
 
+	function uploadVideo(string memory _videoHash, string memory _title) public {
+		// Make sure the video hash exists
+		require(bytes(_videoHash).length > 0);
 
-  constructor() public {
-  }
+		// Make sure video title exists
+		require(bytes(_title).length > 0);
 
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
+		// Make sure uploader address exists
+		require(msg.sender != address(0));
+		// Increment video id
+		videoCount++;
 
-    // Make sure video title exists
+		// Add video to the contract
+		videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
 
-    // Make sure uploader address exists
+		// Trigger an event
+		emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
 
-
-    // Increment video id
-
-    // Add video to the contract
-
-    // Trigger an event
-
-  }
+	}
 }
